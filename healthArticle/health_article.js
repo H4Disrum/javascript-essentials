1621a521872a0669b6cf2ca17248d613d82db107
H4Disrum <dmrakkesh@gmail.com>
2026-04-23 04:58:18 -0400

    sending the request, fetching data

---
+++ healthArticle/health_article.js
@@ -0,0 +1,59 @@
+// creation of xhr
+let xhr = new xmlHtttpRequest(); // creation of xhr
+
+let url = './health_article.json';
+
+// defining the xhr object
+xhr.open('GET', url, true);
+
+//specify response type
+xhr.responseType = 'json';
+
+//Handling the retreived data
+xhr.onload = function () {
+    let articles = xhr.responseType.articles;
+    let articlesDiv = document.getElementById('articles');
+}
+
+//fetching individual data
+articles.forEach(function(article) {
+    var articleDiv = document.createElement('div');
+    articleDiv.classList.add('article');
+
+    var title = document.createElement('h2');
+    title.textContent = article.title;
+
+    var description = document.createElement('p');
+    description.textContent = article.description;
+
+    var waysHeader = document.createElement('h3');
+    waysHeader.textContent = 'Ways to Achieve:';
+
+    var waysList = document.createElement('ul');
+    article.ways_to_achieve.forEach(function(way) {
+      var listItem = document.createElement('li');
+      listItem.textContent = way;
+      waysList.appendChild(listItem);
+    });
+
+    var benefitsHeader = document.createElement('h3');
+    benefitsHeader.textContent = 'Benefits:';
+
+    var benefitsList = document.createElement('ul');
+    article.benefits.forEach(function(benefit) {
+      var listItem = document.createElement('li');
+      listItem.textContent = benefit;
+      benefitsList.appendChild(listItem);
+    });
+
+    articleDiv.appendChild(title);
+    articleDiv.appendChild(description);
+    articleDiv.appendChild(waysHeader);
+    articleDiv.appendChild(waysList);
+    articleDiv.appendChild(benefitsHeader);
+    articleDiv.appendChild(benefitsList);
+
+    articlesDiv.appendChild(articleDiv);
+  });
+
+xhr.send();
